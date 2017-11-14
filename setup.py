8b3db316a1b0dfc2d710209d346e65212543c5ed
Luiz Gois <luiz.gois@luizalabs.com>
2017-11-14 11:34:42 -0200

    fix setup version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 setup(
     name='luizalabs-asyncio-toolkit',
-    version='0.2.0',
+    version='0.2.1',
     description=(
         'The LuizaLabs set of tools '
         'to ease asyncio development'
